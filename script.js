7a5af6f67dd9ddfce53e93f2107d1bda04f5d9fe
usama0print <oussamaacademy@gmail.com>
2024-12-27 06:10:44 +0200

    my first finished website

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".humberger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
